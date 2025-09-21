baa561ae37f5afb82b5aa282d6eb98d30539e597
Mankali Mahesh <112609869+mahimahesh318@users.noreply.github.com>
2025-09-21 12:32:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+function resume() {
+  window.location.href="https://www.canva.com/design/DAGcEaX0mI4/7QtojgTwVjG6eGqjv6RKAQ/view?utm_content=DAGcEaX0mI4&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=ha691cf42ac";
+}
